610ac2c4e56b9d331dbc9f2dcfe608606f55b0b0
Mohammad Reza Taesiri <mtaesiri@gmail.com>
2023-08-24 17:47:15 +0330

    Update content.js

--- src/content.js
+++ src/content.js
@@ -1,20 +1,39 @@
+// content.js
 
-const paperId = window.location.pathname.split('/').pop();
-const audioURL = `https://huggingface.co/datasets/taesiri/arxiv_audio/resolve/main/audio/${paperId}.mp3`;
+const insertAudioPlayer = () => {
+  // Extract the arXiv paper ID from the current URL
+  const paperID = window.location.pathname.split('/abs/')[1];
 
-// Check if the audio file exists
-fetch(audioURL, { method: 'HEAD' })
-  .then(response => {
+  // Form the audio URL based on the paper ID
+  const audioURL = `https://huggingface.co/datasets/taesiri/arxiv_audio/resolve/main/audio/${paperID}.mp3`;
+
+  // Create the audio player element
+  const audioElement = document.createElement('audio');
+  audioElement.controls = true;
+  audioElement.src = audioURL;
+
+  // Style the audio player
+  audioElement.style.width = '100%';  // Set the width to 100% of its container
+  audioElement.style.height = '32px'; // Set a slim height
+  audioElement.style.marginTop = '10px';
+  audioElement.style.marginBottom = '10px';
+
+  // Check if the audio file exists before inserting it
+  fetch(audioURL, { method: 'HEAD' }).then(response => {
     if (response.ok) {
-      // Add an audio player to the page
-      const audioPlayer = document.createElement('audio');
-      audioPlayer.controls = true;
-      audioPlayer.src = audioURL;
-      
-      const targetElement = document.querySelector('h1.title');
-      targetElement.parentNode.insertBefore(audioPlayer, targetElement.nextSibling);
+      // Find the abstract's blockquote element
+      const abstractElement = document.querySelector('blockquote.abstract.mathjax');
+
+      // If found, insert the audio player after the blockquote element
+      if (abstractElement) {
+        abstractElement.parentNode.insertBefore(audioElement, abstractElement.nextSibling);
+      }
     }
-  })
-  .catch(error => {
-    console.error('Error fetching the audio file:', error);
+  }).catch(error => {
+    // Handle any errors, like network issues, here
+    console.error('Error fetching the audio:', error);
   });
+};
+
+// Call the function to insert the audio player
+insertAudioPlayer();
